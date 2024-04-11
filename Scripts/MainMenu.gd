8a9cae96007670f43c1eb6ab589499f38f76be3b
lipefree <43332207+lipefree@users.noreply.github.com>
2024-04-11 21:01:19 +0200

    feature : test level added, button on main menu to access it added. (#69)

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,16 +2,22 @@ extends Control
 
 @onready var startButton : Button = get_node("MarginContainer/VBoxContainer/StartButton")
 @onready var exitButton : Button = get_node("MarginContainer/VBoxContainer/ExitButton")
+@onready var testLevelButton : Button = get_node("MarginContainer/VBoxContainer/TestLevelButton")
 var click = preload("res://Assets/Sounds/SFX/click.wav")
 
 func _ready():
 	startButton.pressed.connect(self._start_pressed)
 	exitButton.pressed.connect(self._exit_pressed)
-
+	testLevelButton.pressed.connect(self._test_level_pressed)
+	
 func _start_pressed():
 	LevelLoader.changeLevel(LevelLoader.firstLevel)
 	SoundManager.play_ui(click)
 
 func _exit_pressed():
 	GameManager.quit()
+	SoundManager.play_ui(click)
+
+func _test_level_pressed():
+	LevelLoader.changeLevel(LevelLoader.testLevel)
 	SoundManager.play_ui(click)
