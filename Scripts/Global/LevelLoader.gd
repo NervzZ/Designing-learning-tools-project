0f2c6ad983766aa1f4cd02c218a54f76f30485d9
willy <lipefree@willys-mbp.home>
2024-04-11 20:40:19 +0200

    feature : test level added, button on main menu to access it added.

--- Scripts/Global/LevelLoader.gd
+++ Scripts/Global/LevelLoader.gd
@@ -2,6 +2,7 @@ extends Node
 
 #Levels global variables
 var firstLevel = preload("res://Levels/FirstLevel.tscn");
+var testLevel = preload("res://Levels/TestLevel.tscn");
 
 func changeLevel(level: PackedScene):
 	get_tree().change_scene_to_packed(level)
