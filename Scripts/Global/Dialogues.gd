81a5904ba34b395b709ac953b59d9f20f6039196
NervzZ <56892303+NervzZ@users.noreply.github.com>
2024-05-02 14:34:04 +0200

    done implemented the feature + example present (#101)

--- Scripts/Global/Dialogues.gd
+++ Scripts/Global/Dialogues.gd
@@ -20,20 +20,44 @@ func greenObstacleHit_b():
 
 var welcomePrompt = Prompt.new(
 	"Hello and welcome to the M-Project! This is an example prompt for testing purposes but saying
-	hello world is way too boring at this point.",
+	hello world is way too boring at this point. Click Ok if you understood!",
 	[
-		{"text": "Ok!", "method": self.welcomePrompt_a}
+		{"text": "Ok!", "method": self.welcomePrompt_a},
+		{"text": "Nope", "method": self.welcomePrompt_b}
 	]
 )
 
 func welcomePrompt_a():
 	GameManager.closePrompt()
+	GameState.boolStates["SaidYesOnExamplePrompt"] = true
+	GameManager.displayPrompt(testPropmptConditional)
+	
+func welcomePrompt_b():
+	GameManager.closePrompt()
+	GameState.boolStates["SaidYesOnExamplePrompt"] = false
+	GameManager.displayPrompt(testPropmptConditional)
+
+#Example of how to use conditions with 'display' and 'inverse' optiomal members.
+#Look at welcomePrompt to understand how to change boolean states
+#If inverse = true, it will invert the display logic. E.g if display = false but inverse = true then we display
+var testPropmptConditional = Prompt.new(
+	"Did you click ok?",
+	[
+		{"text": "Yes", "method": self.testPropmptConditional_close, 'display': "SaidYesOnExamplePrompt"},
+		{"text": "Nope", "method": self.testPropmptConditional_close, 'display': "SaidYesOnExamplePrompt", 'inverse': true}
+	]
+)
+
+func testPropmptConditional_close():
+	GameManager.closePrompt()
+	
 	
 var talkToTestNPCPrompt = Prompt.new(
 	"Hello, this is a test dialog !",
 	[
 		{"text": "Ok!", "method": self.talkToTestNPCPrompt_ok}
-	]
+	],
+	PromptSprites.exampleTalker
 )
 
 func talkToTestNPCPrompt_ok():
--- Scripts/Struct/Prompt.gd
+++ Scripts/Struct/Prompt.gd
@@ -1,9 +1,11 @@
 # Prompt.gd
 class_name Prompt
 
+var talker: Texture2D
 var text: String
 var choices: Array = []
 
-func _init(_text: String, _choices: Array):
+func _init(_text: String, _choices: Array, _talker: Texture = PromptSprites.defaultTalker):
+	talker = _talker
 	text = _text
 	choices = _choices
