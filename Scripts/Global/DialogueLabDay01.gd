cab89cdbb661898f3877dadee25995acdcc79ce3
Simon Thür <thuer.simon@hotmail.com>
2024-05-09 15:10:11 +0200

    add sprites (not correct ones but that can be fixed later)

--- Scripts/Global/DialogueLabDay01.gd
+++ Scripts/Global/DialogueLabDay01.gd
@@ -1,25 +1,24 @@
 extends Node
 
-
-
-
 ################################################################################
 # Welcome message
 ################################################################################
 func welcomeToLabDialogue(): # <- Call this for welcome message
 	GameManager.displayPrompt(DialogueLabDay01.welcomeToLab)
 	
-var welcomeToLab = Prompt.new(
+var welcomeToLab = Prompt.new(  # said by you (or narrator?)
 	"We are about to make a breakthrough in Teleportation.",
 	[
 		{"text":"Ok", "method" : self.welcomeToLab_ok}
-	]
+	],
+	PromptSprites.playerTalker
 )
-var welcomeToLabStartWork = Prompt.new(
+var welcomeToLabStartWork = Prompt.new(  # said by you
 	"Allright, I have a dream. Time to work to make it a reality!",
 	[
 		{"text":"Ok", "method" : GameManager.closePrompt}
-	]
+	],
+	PromptSprites.playerTalker
 )
 
 func welcomeToLab_ok():
@@ -28,25 +27,40 @@ func welcomeToLab_ok():
 
 
 ################################################################################
-# First discussion with NPC
+# First discussion with NPC (info about time limits and others getting close)
+# Discussion with `scientist01Talker`
 ################################################################################
-func dicussTechRace():  # <- Call this for dialogue
-	GameManager.displayPrompt(firstDiscussionWithNPC)
-	
-var firstDiscussionWithNPC = Prompt.new(
+func dicussTechRace():  # <- CALL this for dialogue
+	if GameState.boolStates["TalkedAboutTechnologyRace"]:
+		GameManager.displayPrompt(firstDiscussionWithNPC_after)
+	else:
+		GameManager.displayPrompt(firstDiscussionWithNPC)
+
+func firstDiscussionWithNPC_ok():
+	GameManager.closePrompt()
+	GameState.boolStates["TalkedAboutTechnologyRace"] = true
+	GameManager.displayPrompt(firstDiscussionWithNPC_techRace)
+var firstDiscussionWithNPC = Prompt.new( # said by scientist friend
 	"Hey! Glad you stopped by, we really need to hurry up our development,
 	 otherwise the other teams are gonna beat us to the first functional teleporter!",
 	[
 		{"text":"I know, but we're super close now","method": self.firstDiscussionWithNPC_ok}
-	]
+	],
+	PromptSprites.scientist01Talker
 )
-var firstDiscussionWithNPD_techRace = Prompt.new(
+var firstDiscussionWithNPC_techRace = Prompt.new(  # said by scientist friend
 	"I sure hope so, this is turning into a real technology race...",
 	[
 		{"text":"I know, but we're super close now","method": GameManager.closePrompt}
-	]
+	],
+	PromptSprites.scientist01Talker
 )
-func firstDiscussionWithNPC_ok():
-	GameManager.closePrompt()
-	GameState.boolStates["TalkedAboutTechnologyRace"] = true
-	GameManager.displayPrompt(firstDiscussionWithNPD_techRace)
+var firstDiscussionWithNPC_after= Prompt.new( 
+	"Glad we talked about that.",
+	[
+		{"text":"Leave","method": GameManager.closePrompt}
+	],
+	PromptSprites.scientist01Talker
+)
+
+
--- Scripts/Global/PromptSprites.gd
+++ Scripts/Global/PromptSprites.gd
@@ -2,3 +2,5 @@ extends Node
 
 var defaultTalker: Texture2D = preload("res://Assets/Icons/question.png")
 var exampleTalker: Texture2D = preload("res://icon.svg")
+var playerTalker: Texture2D = preload("res://Assets/SpriteCharactTmp/player.png") #TODO use correct img
+var scientist01Talker: Texture2D = preload("res://Assets/Sprites/Character/Scientist1.png")#TODO use correct img 
