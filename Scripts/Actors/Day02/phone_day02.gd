12664152f220f76fc9a677d0434fb42f8b48d806
lipefree <43332207+lipefree@users.noreply.github.com>
2024-05-24 18:59:21 +0200

    Added ringtone (#169)
    * feature: added ring to phone on day 2 morning

--- Scripts/Actors/Day02/phone_day02.gd
+++ Scripts/Actors/Day02/phone_day02.gd
@@ -1,4 +1,24 @@
 extends Interactable
 
+@export var ringin = true
+var ringing_sound = preload("res://Assets/Sounds/SFX/mixkit-old-telephone-ring-1357.wav")
+var ringing_condition = "PhonedWithArmy"
+@onready var player = get_tree().get_nodes_in_group("Player")[0]
+
 func _trigger_action():
+	SoundManager.stop_ambient()
+	ringin = false
 	DialogueDay02Home.discuss_entry_army_phonecall()
+
+func _physics_process(delta):
+	print("ringing " + str(ringin))
+	if ringin:
+		var distance = global_position.distance_to(player.global_position)
+
+		var maxDistance = 5000
+		var volume = 1.0 - clamp(distance / maxDistance, 0.0, 1.0)
+		# Time loop for ringing
+		if $Timer.is_stopped(): 
+			SoundManager.play_ambient(ringing_sound, 
+										50 * log(volume + 0.1) - 10)
+			$Timer.start(3)
